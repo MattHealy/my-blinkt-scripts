519a82975db3a675b02a5f39cfc990dbba0acd08
Matt Healy <healmatt@gmail.com>
2018-01-29 20:06:36 +0800

    Initial commit

---
+++ back-and-forth.py
@@ -0,0 +1,28 @@
+from blinkt import set_pixel, set_brightness, show, clear
+import time
+
+set_brightness(0.1)
+
+SLEEP = 0.05
+
+PURPLE = (128, 0, 128)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+COLOURS = [PURPLE, RED, GREEN, BLUE]
+
+
+while True:
+
+    for col in COLOURS:
+        for i in range(8):
+            clear()
+            set_pixel(i, col[0], col[1], col[2])
+            show()
+            time.sleep(SLEEP)
+        for i in reversed(range(8)):
+            clear()
+            set_pixel(i, col[0], col[1], col[2])
+            show()
+            time.sleep(SLEEP)
